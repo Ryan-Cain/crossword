b7079e7029e029dac04603582f02eff111ff32c2
Ryan-Cain <rcain93@hotmail.com>
2024-11-17 02:11:04 -0600

    Getting logic in place to add clues and styles to associated and connected tiles. (Will need a serious refactor to clean up later)

--- lib/crossword/Puzzle.ex
+++ lib/crossword/Puzzle.ex
@@ -1,29 +1,26 @@
 defmodule Crossword.Puzzle do
-
   def get_grid(puzzle) do
     Enum.zip(puzzle["grid"], puzzle["gridnums"])
   end
 
   def create_rows(grid, grid_size) do
-    # grid_size = grid["size"]["cols"]
-    # grid_size = grid.size.cols
     grid_builder =
       Enum.reduce(grid, %{full_grid: [], curr_row: [], counter: 0}, fn tile, grid_build ->
-        case grid_build.counter do
-          ^grid_size ->
-            grid_build
-            |> Map.put(:full_grid, grid_build.full_grid ++ [grid_build.curr_row])
-            |> Map.put(:counter, 1)
-            |> Map.put(:curr_row, [%{letter: elem(tile, 0), clue_num: elem(tile, 1), guess: ""}])
-
-          x when x <= grid_size - 1 ->
-            grid_build
-            |> Map.put(:counter, grid_build.counter + 1)
-            |> Map.put(
-              :curr_row,
-              grid_build.curr_row ++
-                [%{letter: elem(tile, 0), clue_num: elem(tile, 1), guess: ""}]
-            )
+        if grid_build.counter == grid_size do
+          grid_build
+          |> Map.put(:full_grid, grid_build.full_grid ++ [grid_build.curr_row])
+          |> Map.put(:counter, 1)
+          |> Map.put(:curr_row, [
+            %{letter: elem(tile, 0), clue_num: elem(tile, 1), guess: "", style: ""}
+          ])
+        else
+          grid_build
+          |> Map.put(:counter, grid_build.counter + 1)
+          |> Map.put(
+            :curr_row,
+            grid_build.curr_row ++
+              [%{letter: elem(tile, 0), clue_num: elem(tile, 1), guess: "", style: ""}]
+          )
         end
       end)
 
@@ -35,18 +32,137 @@ defmodule Crossword.Puzzle do
     grid_builder.full_grid
   end
 
-  def parse_questions(questions) do
-    parsed =
-      Enum.reduce(questions, [], fn something, question ->
-        question_split = String.split(elem(something, 1), ".", parts: 2)
-
-        [
-          {List.first(question_split),
-           {elem(something, 0), String.trim(List.last(question_split))}}
-          | question
-        ]
+  def format_rows(grid, grid_size) do
+    grid_builder =
+      Enum.reduce(grid, %{full_grid: [], curr_row: [], counter: 0}, fn tile, grid_build ->
+        if grid_build.counter == grid_size do
+          grid_build
+          |> Map.put(:full_grid, grid_build.full_grid ++ [grid_build.curr_row])
+          |> Map.put(:counter, 1)
+          |> Map.put(:curr_row, [tile])
+        else
+          grid_build
+          |> Map.put(:counter, grid_build.counter + 1)
+          |> Map.put(
+            :curr_row,
+            grid_build.curr_row ++
+              [tile]
+          )
+        end
       end)
 
-    Enum.reverse(parsed)
+    grid_builder =
+      if length(grid_builder.curr_row) > 0 do
+        Map.put(grid_builder, :full_grid, grid_builder.full_grid ++ [grid_builder.curr_row])
+      end
+
+    grid_builder.full_grid
+  end
+
+  def combine_questions_and_answers(puzzle) do
+    across_clues_and_answers = Enum.zip(puzzle["clues"]["across"], puzzle["answers"]["across"])
+    down_clues_and_answers = Enum.zip(puzzle["clues"]["down"], puzzle["answers"]["down"])
+
+    %{
+      across: parse_questions(across_clues_and_answers),
+      down: parse_questions(down_clues_and_answers)
+    }
+  end
+
+  def parse_questions(questions) do
+    parsed =
+      Enum.reduce(questions, %{}, fn something, question ->
+        question_split = String.split(elem(something, 0), ".", parts: 2)
+
+        Map.put(question, List.first(question_split), %{
+          answer: elem(something, 1),
+          question: elem(something, 0)
+        })
+      end)
+
+    parsed
+  end
+
+  # goes through each element in the grid, and checks if element has a clue num,
+  # if it does it
+  def assign_clue_to_tiles(grid, grid_col_size, clues, direction, index) do
+    increment =
+      if direction == :across do
+        1
+      else
+        grid_col_size
+      end
+
+    element = Enum.at(grid, index)
+
+    grid =
+      if element.clue_num > 0 do
+        IO.inspect(element, label: "element at assign clue to tiles")
+        across_answer = clues.across[to_string(element.clue_num)].answer
+        across_answer_as_list = String.split(across_answer, "", trim: true)
+        IO.inspect(across_answer_as_list, label: "across answer as list")
+        down_answer = clues.down[to_string(element.clue_num)].answer
+        down_answer_as_list = String.split(down_answer, "", trim: true)
+        across_clue = clues.across[to_string(element.clue_num)]
+        down_clue = clues.down[to_string(element.clue_num)]
+
+        across_grid =
+          if not is_nil(across_clue) do
+            apply_word(grid, increment, across_answer_as_list, index, across_clue)
+          else
+            grid
+          end
+
+        down_grid =
+          if not is_nil(down_clue) do
+            apply_word(across_grid || grid, increment, down_answer_as_list, index, down_clue)
+          else
+            if not is_nil(across_grid) do
+              across_grid
+            else
+              grid
+            end
+
+            grid
+          end
+
+        down_grid
+      else
+        grid
+      end
+
+    IO.inspect(grid, label: "grid after recursion!!")
+    grid
+  end
+
+  def apply_word(grid, increment, answer_as_list, index, clue) do
+    element = Enum.at(grid, index)
+    IO.inspect(element, label: "apply word element")
+    IO.inspect(index, label: "apply word index")
+    IO.inspect(clue, label: "apply word clues")
+
+    if not is_nil(answer_as_list) and length(answer_as_list) > 0 do
+      [_curr_letter | shortened_answer_as_list] = answer_as_list
+      IO.inspect(element, label: "apply word, element in if not")
+
+      styles =
+        cond do
+          length(answer_as_list) == String.length(clue.answer) ->
+            "selected-tile-tbl"
+
+          length(answer_as_list) == 1 ->
+            "selected-tile-tbr"
+
+          true ->
+            "selected-tile-tb"
+        end
+
+      new_element = Map.put(element, :style, styles)
+      new_grid = List.replace_at(grid, index, new_element)
+      apply_word(new_grid, increment, shortened_answer_as_list, index + increment, clue)
+    else
+      IO.inspect(label: "return grid")
+      grid
+    end
   end
 end
--- lib/crossword_web/live/crossword_live/index.ex
+++ lib/crossword_web/live/crossword_live/index.ex
@@ -11,12 +11,29 @@ defmodule CrosswordWeb.CrosswordLive do
     puzzle =
       CrosswordAPI.get_puzzle(url)
 
+    clues = Puzzle.combine_questions_and_answers(puzzle)
     initial_grid = Puzzle.get_grid(puzzle)
+    # IO.inspect(initial_grid, label: "initial grid")
+
     formatted_grid = Puzzle.create_rows(initial_grid, puzzle["size"]["cols"])
+    # IO.inspect(formatted_grid, label: "formatted grid")
+    # IO.inspect(Enum.concat(formatted_grid))
+
+    assigned =
+      Puzzle.assign_clue_to_tiles(
+        Enum.concat(formatted_grid),
+        puzzle["size"]["cols"],
+        clues,
+        :across,
+        0
+      )
+
+    reformatted = Puzzle.format_rows(assigned, puzzle["size"]["cols"])
+    IO.inspect(reformatted, label: "reformatted")
 
     {:ok,
      assign(socket,
-       grid: formatted_grid,
+       grid: reformatted,
        clues_across: puzzle["clues"]["across"],
        clues_down: puzzle["clues"]["down"]
      )}
@@ -24,12 +41,12 @@ defmodule CrosswordWeb.CrosswordLive do
 
   def render(assigns) do
     ~H"""
-    <div class="flex justify-around w-screen">
+    <div class="flex justify-around w-screen mt-8">
       <div class="bg-light-blue" class="flex flex-col">
         <div :for={row <- @grid} class="flex">
           <div
             :for={tile <- row}
-            class="h-14 w-14 border-2 border-black relative flex justify-center items-center"
+            class={"h-14 w-14 border-black border-2 relative flex justify-center items-center #{tile.style}"}
           >
             <span :if={tile.clue_num > 0} class="absolute top-0 left-0 text-sm">
               <%= tile.clue_num %>
